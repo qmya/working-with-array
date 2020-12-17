4d3600a1f5a5d185870681f7b401333ed35c9c17
qmya <qandeelgul90@gmail.com>
2020-12-17 09:39:50 -0500

    simple Array SLICE method

---
+++ script.js
@@ -0,0 +1,35 @@
+"use strict";
+
+//Array METHODS LECTURE: 😀
+//Why array have methods :
+//cause methods are the function
+//Array are objects / function and they they get access to the special buildin methods
+
+let arr = ["a", "b", "c", "d", "e"];
+
+//FIRST SLICE METHOD 🥒
+//We can take the slice of an array without changing the original array
+
+const newSliceArr = arr.slice(2); //here the slice will starts from index 2 all the way to the end
+console.log(newSliceArr); //newSliceArr = ["c", "d", "e"]
+
+//giving the end parimeter too
+//just like string the end perimeter is 3-1 = 2 . Here at 2 index is "c"
+const smallSliceArr = arr.slice(1, 3);
+console.log(smallSliceArr); //smallSliceArr = ["b", "c"]
+
+//MINUS
+//here the array will starts from the end of the array, from e
+let minusSliceArr = arr.slice(-2);
+console.log(minusSliceArr); //['d','e'];
+minusSliceArr = arr.slice(-1);
+console.log(minusSliceArr); //['e'];
+minusSliceArr = arr.slice(-3);
+console.log(minusSliceArr); //["c", "d", "e"]
+
+//Let says the negative ending position
+let negativeEndSliceArr = arr.slice(1, -2);
+//here it starts from the position/index 1
+//but for the end we do : 1-(-2)= 1+2=3
+//or you can say it exract everything except the last two
+console.log(negativeEndSliceArr); //['b', 'c'];
